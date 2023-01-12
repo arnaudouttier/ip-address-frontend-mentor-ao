cf58d174fe363f62c2e676e53d3c1a96a7046cec
aouttier.ext <arnaud.outtier@protonmail.com>
2023-01-12 17:51:34 +0100

    button

--- src/components/IpApiInput.vue
+++ src/components/IpApiInput.vue
@@ -28,7 +28,7 @@ onMounted(() => {
       <h1>IP Address Tracker</h1>
 
       <div class="search">
-        <input type="text" value="IP" placeholder="Search for any IP address or domain">
+        <input type="text" value="" placeholder="Search for any IP address or domain">
         <button class="btn btn-search">
           <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
             <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
